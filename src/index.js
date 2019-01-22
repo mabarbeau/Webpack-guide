3ea719900eef003f82f1f2d3d2f7d7a863b211bf
Marc Barbeau <mabarbeau1@gmail.com>
2019-01-22 09:40:45 -0800

    2.6 Wrapping up
    https://webpack.js.org/guides/asset-management/#wrapping-up

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
 
 function component() {
   let element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }
