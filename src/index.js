6d74c09e47157761f9f709359a5db2817c9056c2
Marc Barbeau <mabarbeau1@gmail.com>
2019-01-22 09:16:44 -0800

    2.2 Loading CSS
    https://webpack.js.org/guides/asset-management/#loading-css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  element.classList.add('hello');
 
   return element;
 }
