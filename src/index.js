6aaf49c920c193b1157855357384636340599cbb
Marc Barbeau <mabarbeau1@gmail.com>
2019-01-23 16:40:26 -0800

    5.3 Gotchas
    https://webpack.js.org/guides/hot-module-replacement/#gotchas

--- src/index.js
+++ src/index.js
@@ -15,11 +15,14 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
 
 if (module.hot) {
   module.hot.accept('./print.js', function() {
     console.log('Accepting the updated printMe module!');
-    printMe();
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
   })
 }
